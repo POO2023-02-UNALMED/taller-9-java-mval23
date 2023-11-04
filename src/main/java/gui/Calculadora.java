de11954944c545b85f5c50bebad86cbbac4ac8cc
mval23 <111825282+mval23@users.noreply.github.com>
2023-11-04 12:02:33 -0500

    Calculadora :blush:

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -7,19 +7,19 @@ import javafx.stage.Stage;
 
 public class Calculadora extends Application  {
 	
-	public static void main(String args[]){ 
-	      ***(args); 
+	public static void main(String args[]){
+		launch(args);
 	} 
 	
 	@Override
 	public void start(Stage window) throws Exception {
-		*** vb = new VBox();
-		window.setTitle(***);
+		VBox vb = new VBox();
+		window.setTitle("Calculadora");
 		
 		vb.getChildren().add(new Calculator());
-		Scene sc = new Scene(***, 270, 270);
-		window.setScene(***);
-		***.show();
+		Scene sc = new Scene(vb, 270, 270);
+		window.setScene(sc);
+		window.show();
 	}
 	
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens gui to javafx.fxml;
+    exports gui;
+}
